4674d438cd974d45abafe1d089df7320bb234726
Mostafa Elfallal <mostafaelfallal90@gmail.com>
2021-08-12 19:39:39 +0200

    Update CountSort.c

--- CountingSort/CountSort.c
+++ CountingSort/CountSort.c
@@ -10,31 +10,17 @@
 void CountingSort(int A[],int B[],int k,int A_Size)
 {
 	int * C = (int *) calloc((k+1),sizeof(int));
-	for(int i=0;i<6;i++){
-			printf("%d ",C[i]);
-	}
-	printf("\n");
+
 	for(int i=0; i < A_Size ; i++){
 		C[A[i]]++;
 	}
-	for(int i=0;i<6;i++){
-				printf("%d ",C[i]);
-	}
-	printf("\n");
+
 	for(int i = 1; i<=k;i++){
 		C[i] += C[i-1];
 	}
-	for(int i=0;i<6;i++){
-				printf("%d ",C[i]);
-	}
-	printf("\n");
 	for(int i =A_Size-1 ; i>=0;i-- )
 	{
 		B[C[A[i]]-1] = A[i];
 		C[A[i]]--;
 	}
-	for(int i=0;i<8;i++){
-				printf("%d ",B[i]);
-	}
-	printf("\n");
 }

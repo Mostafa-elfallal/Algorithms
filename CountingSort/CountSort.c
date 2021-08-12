f4d58daf01f7d425655c28716a8c549b64c3de69
Mostafa Elfallal <mostafaelfallal90@gmail.com>
2021-08-12 19:38:58 +0200

    Add files via upload

---
+++ CountingSort/CountSort.c
@@ -0,0 +1,40 @@
+/*
+ * CountSort.c
+ *
+ *  Created on: Aug 12, 2021
+ *      Author: Mostafa ElFallal
+ */
+#include "CountSort.h"
+#include "stdlib.h"
+#include "stdio.h"
+void CountingSort(int A[],int B[],int k,int A_Size)
+{
+	int * C = (int *) calloc((k+1),sizeof(int));
+	for(int i=0;i<6;i++){
+			printf("%d ",C[i]);
+	}
+	printf("\n");
+	for(int i=0; i < A_Size ; i++){
+		C[A[i]]++;
+	}
+	for(int i=0;i<6;i++){
+				printf("%d ",C[i]);
+	}
+	printf("\n");
+	for(int i = 1; i<=k;i++){
+		C[i] += C[i-1];
+	}
+	for(int i=0;i<6;i++){
+				printf("%d ",C[i]);
+	}
+	printf("\n");
+	for(int i =A_Size-1 ; i>=0;i-- )
+	{
+		B[C[A[i]]-1] = A[i];
+		C[A[i]]--;
+	}
+	for(int i=0;i<8;i++){
+				printf("%d ",B[i]);
+	}
+	printf("\n");
+}
---
+++ CountingSort/CountSort.h
@@ -0,0 +1,13 @@
+/*
+ * CountSort.h
+ *
+ *  Created on: Aug 12, 2021
+ *      Author: Mostafa ElFallal
+ */
+
+#ifndef COUNTSORT_H_
+#define COUNTSORT_H_
+
+void CountingSort(int A[],int B[],int k,int A_Size);
+
+#endif /* COUNTSORT_H_ */
